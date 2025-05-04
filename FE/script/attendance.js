b064d0a9cff1b45e1eeb4f047b12518e1f945f9a
Tên của bạn <hungnguyen23072004@gmail.com>
2025-05-04 21:33:11 +0700

    alo

---
+++ FE/script/attendance.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const routes = {
+        'account': 'admin.html',
+        'employee': 'employee.html',
+        'payroll': 'payroll.html',
+        'attendance': 'attendance.html',
+    };
+    
+    // Duyệt qua từng phần tử trong menu và thêm sự kiện click
+    Object.keys(routes).forEach(id => {
+        const el = document.getElementById(id);
+        if (el) {
+            el.style.cursor = 'pointer'; // Hiện thị dấu tay khi rê chuột
+            el.addEventListener('click', () => {
+                window.location.href = routes[id]; // Chuyển trang khi click vào menu
+            });
+        }
+    });
+        
+        });
