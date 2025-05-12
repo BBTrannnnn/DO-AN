70d9c5c968153fb4046b537e1f1f21aab722980f
Hưng Nè <126441579+BBTrannnnn@users.noreply.github.com>
2025-05-12 19:28:22 +0700

    Update payrolls.py

--- BE/models/payrolls.py
+++ BE/models/payrolls.py
@@ -6,10 +6,11 @@ class Payroll(db.Model):
     __tablename__ = 'payroll'
 
     id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.String(10), db.ForeignKey('employees.id'), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable=False)
     salary = db.Column(db.Numeric(10, 3))  # 
-    time = db.Column(db.String(7))  # Dạng MM/YYYY, ví dụ: '03/2025'
+    time = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
-        return f"<Payroll {self.employee_id} - {self.time} - {self.salary}>"
+        
+        return f"<Payroll {self.employee_id} - {self.time.strftime('%d/%m/%Y')} - {self.salary}>"
 
